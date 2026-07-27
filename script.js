3290d2afbad18a1cba3d07aa10eeb06254db4374
Sudharshan <prawjalhs@gmail.com>
2026-07-27 14:04:50 +0530

    Add destination section

--- script.js
+++ script.js
@@ -48,3 +48,34 @@ muteBtn.addEventListener("click", () => {
     videoPlayer.muted = !videoPlayer.muted;
     muteBtn.textContent = videoPlayer.muted ? "🔇" : "🔊";
 });
+
+//*---------destination---------*//
+
+var destinationSlider = new Swiper('.destination-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2.5,
+        slideShadows: false,
+    },
+
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+    speed: 800,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
